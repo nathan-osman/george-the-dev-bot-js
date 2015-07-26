6b6d4654967555c9d08778daeee7a18c211f3284
Nathan Osman <nathan@quickmediasolutions.com>
2015-07-25 23:43:34 -0700

    Added login() function.

---
+++ login.js
@@ -0,0 +1,44 @@
+/**
+ * George the Dev - Stack Exchange Chatbot
+ * Copyright 2015 - Nathan Osman
+ */
+
+var webpage = require('webpage');
+
+// In order to access the chat pages, the user must first log into their
+// Stack Exchange account. This consists of entering email/password and
+// proceeding through the confirmation steps. The callback will be invoked
+// when the login succeeds.
+exports.login = function(callback) {
+
+    // Load the initial login page
+    var loginPage = webpage.create();
+
+    // Process each step of the login process
+    loginPage.onLoadFinished = function(status) {
+
+        // Check for an error
+        if(status != 'success') {
+            console.log("Unable to display login page.");
+            phantom.exit();
+        }
+
+        // This technique is fragile but it works for now - just keep
+        // feeding in the login credentials and mashing the submit button
+        // until the Stack Exchange homepage is loaded
+        if(loginPage.url == 'http://stackexchange.com/') {
+            loginPage.close();
+            callback();
+        } else {
+            loginPage.switchToFrame('affiliate-signin-iframe');
+            loginPage.evaluate(function(cfg) {
+                $('#email').val(cfg.email);
+                $('#password').val(cfg.password);
+                $('input[type=submit]').click();
+            }, CONFIG);
+        }
+    };
+
+    // Load the page
+    loginPage.open('https://stackexchange.com/users/login#log-in');
+};
